18d1742e51175bdc9c7a2f956949d77899293cfd
dg <dg@shadowrobot.com>
2017-08-03 22:55:29 +0100

    button signals ariving

--- urnaipsum.py
+++ urnaipsum.py
@@ -10,7 +10,7 @@ import threading
 port = "/dev/ttyUSB0"
 baud = 9600
 
-incoming = serial.Serial(port, baud, timeout=10)
+incoming = serial.Serial(port, baud, timeout=100)
 
 mutex = threading.Lock()
 button_state = 0
@@ -18,32 +18,33 @@ button_state = 0
 def listen_to_button():
     while True:
         message = incoming.readline()
-
         if message == "on\r\n":
             mutex.acquire()
             button_state = True
+            print "on"
             mutex.release()
 
         elif message == "off\r\n":
             mutex.acquire()
             button_state = False
+            print "off"
             mutex.release()
 
-def main():
-    thread = threading.Thread(target=listen_to_button)
-    thread.start()
 
-    player = remote.VLC()
+thread = threading.Thread(target=listen_to_button)
+thread.start()
 
-    player.add("/home/dg/Downloads/echo-hereweare.mp4")
-    player.play()
-    sleep(0.1)
-    player.x("f on")
-    sleep(5)
+player = remote.VLC()
 
-    player.clear()
-    player.add("/home/dg/Downloads/big_buck_bunny.mp4")
-    player.play()
+player.add("/home/dg/Downloads/echo-hereweare.mp4")
+player.play()
+sleep(0.1)
+player.x("f on")
+sleep(5)
+
+player.clear()
+player.add("/home/dg/Downloads/big_buck_bunny.mp4")
+player.play()
 
 while True:
     sleep(1)
